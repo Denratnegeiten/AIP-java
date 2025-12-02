989b7d834e1249d89d69509013f4060b6e1bd48d
Denratnegeiten <denratnegeiten@lin.netis.cc>
2025-12-02 22:55:58 +0300

    14

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 "use strict";
-alert('Привет, мир!'); // комментарий
-/*
-	комментарий
+let a = 1;
+let b = 2;
+let c = 3;
 
-    комментарий
-*/
+alert(a);
+alert(b);
+alert(c);
+
+let a1 = 1, b1 = 2, c1 = 3;
+
+alert(a1);
+alert(b1);
+alert(c1);
+
+let a2, b2, c2;
+
+a2 = 1;
+b2 = 2;
+c2 = 3;
+
+alert(a2);
+alert(b2);
+alert(c2);
