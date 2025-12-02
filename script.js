7fec48dede9e75e7612b5253ed0614dedf92bba0
Denratnegeiten <denratnegeiten@lin.netis.cc>
2025-12-02 22:56:22 +0300

    15

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 "use strict";
-let a = 1;
-let b = 2;
-let c = 3;
+let a;
 
+a = 10;
 alert(a);
-alert(b);
-alert(c);
 
-let a1 = 1, b1 = 2, c1 = 3;
-
-alert(a1);
-alert(b1);
-alert(c1);
-
-let a2, b2, c2;
-
-a2 = 1;
-b2 = 2;
-c2 = 3;
-
-alert(a2);
-alert(b2);
-alert(c2);
+a = 20;
+alert(a);
